dbf119eb9609bf48a565eaef2b685e2f837d10bb
PorkerKing <qpiduwiqigdkckywi@gmail.com>
2025-06-16 12:23:53 +0800

    Create auth.ts

---
+++ lib/auth.ts
@@ -0,0 +1,44 @@
+import NextAuth, { type AuthOptions } from 'next-auth';
+import GitHub from 'next-auth/providers/github';
+import Email from 'next-auth/providers/email';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from '@/lib/prisma';
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+    Email({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: Number(process.env.EMAIL_SERVER_PORT),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, user }) {
+      if (session?.user) {
+        const sessionUser = session.user as { id: string; role?: string };
+        sessionUser.id = user.id;
+
+        const clubMember = await prisma.clubMember.findFirst({
+            where: { userId: user.id },
+            select: { role: true }
+        });
+        if (clubMember) {
+            sessionUser.role = clubMember.role;
+        }
+      }
+      return session;
+    },
+  },
+};
